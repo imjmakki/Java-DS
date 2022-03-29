53b5110b77074da45463a0fb0fbac1c67ae4d946
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 18:28:37 +0300

    Complex Data Types

--- src/main/java/app/ds/TheArrays.java
+++ src/main/java/app/ds/TheArrays.java
@@ -3,7 +3,7 @@ package app.ds;
 import java.util.Arrays;
 
 public class TheArrays {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String[] colors = new String[5];
         colors[0] = "Green";
         colors[1] = "Blue";
