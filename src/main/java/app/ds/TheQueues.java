cc77d4f216d1a65deadd08f87e49f257318fcc9a
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-30 01:06:51 +0300

    Queue FIFO(First In First Out)

--- src/main/java/app/ds/TheQueues.java
+++ src/main/java/app/ds/TheQueues.java
@@ -9,6 +9,7 @@ public class TheQueues {
         supermarket.add(new Person("MJ", 26));
         supermarket.add(new Person("Hassan", 20));
         supermarket.add(new Person("Nabeel", 19));
+        supermarket.add(new Person("Ali", 11));
     }
     static record Person(String name, int age) {
 
