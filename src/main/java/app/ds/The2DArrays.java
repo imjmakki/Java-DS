a7d4f1cad2516944c4ba9b8e20410c5ea526b2ee
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 18:05:44 +0300

    2d arrays

--- src/main/java/app/ds/The2DArrays.java
+++ src/main/java/app/ds/The2DArrays.java
@@ -5,7 +5,7 @@ public class The2DArrays {
         char[][] board = new char[3][3];
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j< 3; j++){
-
+                board[i][j] = '-';
             }
         }
     }
