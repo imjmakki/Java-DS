c6a30411d641768b37a28f98771730d652149286
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-30 01:03:33 +0300

    Stack LIFO(Last In First Out)

---
+++ src/main/java/app/ds/TheQueues.java
@@ -0,0 +1,13 @@
+package app.ds;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TheQueues {
+    public static void main(String[] args) {
+        Queue<Person> supermarket = new LinkedList<>();
+    }
+    static record Person(String name, int age) {
+
+    }
+}
