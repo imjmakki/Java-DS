b768066c77859d15fef56db900a08e950a3b97b1
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 17:37:58 +0300

    Arrays

--- src/main/java/app/ds/Arrays.java
+++ src/main/java/app/ds/Arrays.java
@@ -2,6 +2,10 @@ package app.ds;
 
 public class Arrays {
     public static void main(String[] args){
+        String[] colors = new String[5];
+        colors[0] = "Green";
+        colors[1] = "Blue";
 
+        System.out.println(colors);
     }
 }
