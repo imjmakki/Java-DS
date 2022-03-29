bc5da6b04dabf946e139ba016aaf689fd519c981
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 18:29:45 +0300

    Complex Data Types

--- src/main/java/app/ds/TheLists.java
+++ src/main/java/app/ds/TheLists.java
@@ -1,7 +1,10 @@
 package app.ds;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TheLists {
     public static void main(String[] args) {
-
+        List colors = new ArrayList();
     }
 }
