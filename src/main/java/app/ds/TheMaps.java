e687b8101cf92fb43169437eda19917c8bdf3568
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-30 01:43:19 +0300

    Queue FIFO(First In First Out)

--- src/main/java/app/ds/TheMaps.java
+++ src/main/java/app/ds/TheMaps.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class TheMaps {
     public static void main(String[] args) {
         Map<Integer, Person> map = new HashMap<>();
+        map.put(1, new Person("Ali"));
     }
 
     record Person(String name) {}
