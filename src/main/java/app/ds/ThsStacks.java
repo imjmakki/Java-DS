e1a356481e7c5227be8fbb49549e4f3f7792e313
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-29 18:56:35 +0300

    Stack LIFO(Last In First Out)

--- src/main/java/app/ds/ThsStacks.java
+++ src/main/java/app/ds/ThsStacks.java
@@ -6,5 +6,7 @@ public class ThsStacks {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
+        stack.push(2);
+        stack.push(3);
     }
 }
