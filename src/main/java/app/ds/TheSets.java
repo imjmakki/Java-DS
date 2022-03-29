ab1c7d28baf989613cb6becd7384f86f0d8befda
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-30 01:30:55 +0300

    Queue FIFO(First In First Out)

--- src/main/java/app/ds/TheSets.java
+++ src/main/java/app/ds/TheSets.java
@@ -1,10 +1,11 @@
 package app.ds;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class TheSets {
     public static void main(String[] args) {
-        Set<Ball> balls =
+        Set<Ball> balls = new HashSet<>();
     }
 
     record Ball(String color) {}
