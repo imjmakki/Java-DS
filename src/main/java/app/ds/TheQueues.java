145844395b6f8167ee635b53a27186ccf3f28150
Mohamd J Alanbaki <mehmet.anbaki@gmail.com>
2022-03-30 01:05:54 +0300

    Queue FIFO(First In First Out)

--- src/main/java/app/ds/TheQueues.java
+++ src/main/java/app/ds/TheQueues.java
@@ -6,6 +6,9 @@ import java.util.Queue;
 public class TheQueues {
     public static void main(String[] args) {
         Queue<Person> supermarket = new LinkedList<>();
+        supermarket.add(new Person("MJ", 26));
+        supermarket.add(new Person("Hassan", 20));
+        supermarket.add(new Person("Nabeel", 19));
     }
     static record Person(String name, int age) {
 
